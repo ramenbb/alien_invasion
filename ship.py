0ee6eb614ad0adb61c87279ae02e7a38aadde9e5
ramenbb <andrewhsun18@gmail.com>
2024-12-16 23:31:20 -0500

    trying to add up and down controls

--- ship.py
+++ ship.py
@@ -18,23 +18,33 @@ class Ship():
 		self.rect.bottom = self.screen_rect.bottom
 
 		# Store a decimal value for the ship's center.
-		self.center = float(self.rect.centerx)
+		self.centerx = float(self.rect.centerx)
+		self.centery = float(self.rect.centery)
 
 		# Movement flag
 		self.moving_right = False
 		self.moving_left = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		"""Update the ship's position based on the movement flag."""
 		# Update the ship's center value, not the rect
 		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.center += self.ai_settings.ship_speed_factor
+			self.centerx += self.ai_settings.ship_speed_factor
 
 		if self.moving_left and self.rect.left > self.screen_rect.left:
-			self.center -= self.ai_settings.ship_speed_factor
+			self.centerx -= self.ai_settings.ship_speed_factor
+
+		if self.moving_up and self.rect.top < self.screen_rect.top:
+			self.centery += self.ai_settings.ship_speed_factor
+
+		if self.moving_down and self.rect.bottom > self.screen_rect.bottom:
+			self.centery -= self.ai_settings.ship_speed_factor
 
 		# Update rect object from self.center.
-		self.rect.centerx = self.center
+		self.rect.centerx = self.centerx
+		self.rect.centery = self.centery
 
 	def blitme(self):
 		"""Draw the ship at its current location."""
